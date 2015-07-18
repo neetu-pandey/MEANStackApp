92ce1aeaa9a4a43006d47d91dc80670ff30e32a9
Neetu Pandey <neetu.pandey@sungard.com>
2015-07-18 07:11:46 +0530

    Added stylus and jade changes

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var stylus = require('stylus');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
+app.use(logger('dev'));
+app.use(bodyParser());
+
+app.get('*', function(req, res) {
+   res.render('index');
+});
+
+var port = 3030;
+app.listen(port);
+console.log('Listening on port... ', port);
